327d64bab77414911fd54ea408e85e93cf9e1d97
IHSAN ANWAR <ihsan.anwar4321@gmail.com>
2025-05-10 14:14:59 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function DarkMode () {
+    document.getElementById ("theme-changer").style.backgroundColor = "#444";
+}
+function LightMode () {
+    document.getElementById ("theme-changer").style.backgroundColor = "#fff";
+}
